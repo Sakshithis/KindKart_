963777d11f99fa0e337c5909190ca4f449eec724
humansamiksha <samikshasharma121920@gmail.com>
2026-04-05 14:52:59 +0530

    db: remove sqlite and switch to postgres

--- config.py
+++ config.py
@@ -4,7 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'kindkart-super-secret-key-123!!'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    db_url = os.environ.get('DATABASE_URL')
+    if db_url and db_url.startswith('postgres://'):
+        db_url = db_url.replace('postgres://', 'postgresql://', 1)
+        
+    SQLALCHEMY_DATABASE_URI = db_url or \
         'sqlite:///' + os.path.join(basedir, 'kindkart.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, 'static', 'images', 'uploads')
